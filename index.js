6929a099926236bfd07d4cb19d64a0aab37d320f
Michi Berger <mberger83@yahoo.de>
2022-01-28 16:38:51 +0100

    Added Navigation.js and tried to toggle the navigation

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 console.clear();
 import Card from './js/Cards.js';
+import Navigation from './js/Navigation.js';
 
 import { logout } from './js/logout.js';
-import { navigation } from './js/navigation.js';
+// import { navigation } from './js/navigation.js';
 
 const allCardElements = document.querySelectorAll('[data-js="card"]');
+const allNavElements = document.querySelectorAll('[data-js="navigation"]');
 
 allCardElements.forEach(cardElement => {
   Card(cardElement);
 });
+allNavElements.forEach(cardElement => {
+  Navigation(cardElement);
+});
 
 logout();
-navigation();
+// navigation();
--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,41 @@
-export function navigation() {
-  
+export default function Navigation(cardElement) {
+  const homeNavButton = cardElement.querySelector('[data-js="nav-home"]');
+  const bookmarkNavButton = cardElement.querySelector(
+    '[data-js="nav-bookmark"]'
+  );
+  const createNavButton = cardElement.querySelector('[data-js="nav-create"]');
+  const profileNavButton = cardElement.querySelector('[data-js="nav-profile"]');
+
+  console.log(homeNavButton);
+  console.log(bookmarkNavButton);
+  console.log(createNavButton);
+  console.log(profileNavButton);
+
+  homeNavButton?.addEventListener('click', () => {
+    homeNavButton.classList.add('nav__link--active');
+    bookmarkNavButton.classList.remove('nav__link--active');
+    createNavButton.classList.remove('nav__link--active');
+    profileNavButton.classList.remove('nav__link--active');
+  });
+
+  bookmarkNavButton?.addEventListener('click', () => {
+    homeNavButton.classList.remove('nav__link--active');
+    bookmarkNavButton.classList.add('nav__link--active');
+    createNavButton.classList.remove('nav__link--active');
+    profileNavButton.classList.remove('nav__link--active');
+  });
+
+  createNavButton?.addEventListener('click', () => {
+    homeNavButton.classList.remove('nav__link--active');
+    bookmarkNavButton.classList.remove('nav__link--active');
+    createNavButton.classList.add('nav__link--active');
+    profileNavButton.classList.remove('nav__link--active');
+  });
+
+  profileNavButton?.addEventListener('click', () => {
+    homeNavButton.classList.remove('nav__link--active');
+    bookmarkNavButton.classList.remove('nav__link--active');
+    createNavButton.classList.remove('nav__link--active');
+    profileNavButton.classList.add('nav__link--active');
+  });
 }
